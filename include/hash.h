d956d0ec9c267c948aa2b115d01abb54047fa9f5
Shriyad29 <shriya.dullur@students.iiit.ac.in>
2021-05-09 00:18:16 +0530

    added comments

--- include/hash.h
+++ include/hash.h
@@ -4,6 +4,10 @@
 #define NAME_LEN 10000
 #define MAX_INPUTS 100000
 
+// struct to store starting intersection / vertex , ending intersection
+// street name and  weight of the street 
+// weight refers to number of cars passing a street
+
 typedef struct edge_unit
 {
     int start_index, end_index;
@@ -11,18 +15,28 @@ typedef struct edge_unit
     char *edge_name;
 } edge;
 
-unsigned long int GetHash(char *name_str);
-
-void Insert(int start_index, int end_index, char name[NAME_LEN], edge *Edge_Table);
-
-void ReadInput_1(int num_streets, edge *Edge_Table, FILE *fptr);
-
-void AddWeight(char name[NAME_LEN], edge *Edge_Table);
-
-void readpart2(int num_cars, edge *Edge_Table, FILE *fptr);
+// Creates a Hash Table of structs
 
 edge *CreateTable();
 
-void Traffic_Output(edge *Edge_Table, FILE *ptr);
+// Generates hash key for a street name
+
+unsigned long int GetHash(char *name_str);
+
+// inserts data of a struct into hash table
+
+void Insert(int start_index, int end_index, char name[NAME_LEN], edge *Edge_Table);
+
+//Reads indexes and edge name as inputs
+
+void ReadInput_1(int num_streets, edge *Edge_Table, FILE *fptr);
+
+// Reads input to calculate weight of edge
+
+void readpart2(int num_cars, edge *Edge_Table, FILE *fptr);
+
+// Calculates weight of an edge
+
+void AddWeight(char name[NAME_LEN], edge *Edge_Table);
 
 #endif
