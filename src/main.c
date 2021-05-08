4f82d5ca0d975d6c6e29adf3bae2ad6df9b05e9b
SriLoukya <sri.loukya@students.iiit.ac.in>
2021-05-08 20:31:00 +0530

    temporary main

--- src/main.c
+++ src/main.c
@@ -7,17 +7,18 @@ int main(void)
 {
     FILE *fptr;
     FILE *ptr;
-    int num_intersections, num_streets, num_cars;
+    int num_intersections, num_streets, num_cars,garbage1,garbage2;
 
     char input[50];
     printf("Enter the input file name\n");
     scanf("%s", input);
     fptr = fopen(input, "r");
-
+    fscanf(fptr, "%d", &garbage1);
     fscanf(fptr, "%d", &num_intersections);
     fscanf(fptr, "%d", &num_streets);
     fscanf(fptr, "%d", &num_cars);
-
+    fscanf(fptr, "%d", &garbage2);
+    
     edge *Edge_Table = CreateTable();
 
     ReadInput_1(num_streets, Edge_Table, fptr);
